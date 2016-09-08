afefe7e5a3bed13bb37c8e49d5f52b1345ff47fc
Nabil Alhassani <nda11@pitt.edu>
2016-09-08 13:54:32 -0400

    commit to ch2-ex1

--- ch2.py/ch2-ex6.py
+++ ch2.py/ch2-ex6.py
@@ -1,7 +1,7 @@
 # Template for code submission
 # name : Nabil Alhassani
 # email : nda11@pitt.edu
-# date :septemper/7th
+# date :septemper/8th
 # class : CS0008-f2016
 # instructor : Max Novelli (man8@pitt.edu)
 # Description:Starting with Python, Chapter 2,
@@ -10,6 +10,3 @@
 # ...and now let's program with Python
 
 # exercise 6
-#This program asks the user to enter the amount of a purchase
-amount_purchase=(float(input('enter the amount of a purchase:' ))
-
