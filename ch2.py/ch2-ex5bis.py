53cd82ba58d11ac934fc53739adcd10c03f34c8f
Nabil Alhassani <nda11@pitt.edu>
2016-09-09 16:54:04 -0400

    commit to ch2-ex 3 and 5 bis

---
+++ ch2.py/ch2-ex5bis.py
@@ -0,0 +1,36 @@
+# Template for code submission
+# name : Nabil Alhassani
+# email : nda11@pitt.edu
+# date :septemper/9th
+# class : CS0008-f2016
+# instructor : Max Novelli (man8@pitt.edu)
+# Description:Starting with Python, Chapter 2,
+# Notes:
+# any notes to the instructor and/or TA goes here
+# ...and now let's program with Python
+
+# exercise 5
+#this program is measuring the distace that a car travels
+# set value to distance variables, speed
+#distance= speed * time
+speed = 90
+
+
+# set value to the time variable
+time1=6
+time2=10
+time3=15
+time4=2.25
+
+# get the distance traveled
+distance1=speed *time1
+distance2=speed *time2
+distance3=speed *time3
+distance4=speed *time4
+
+#Dispaly the distance traveled
+print("traveling at 90Km/h...")
+print("The distance the car will travel in 6 hours",distance1,'km')
+print("The distance the car will travel in 10 hours",distance2,'km')
+print("The distance the car will travel in 15 hours",distance3,'km')
+print("The distance the car will travel in 2 hours and 25 minutes",distance4,'km')
