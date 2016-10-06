c4d956cf5d723d0c185d535d113590c84acd6538
Nabil Alhassani <nda11@pitt.edu>
2016-10-06 14:14:52 -0400

    ch4

---
+++ ch4/ex3.py
@@ -0,0 +1,12 @@
+# name : Nabil Alhassani
+# email : nda11@pitt.edu
+# date :septemper/11th
+# class : CS0008-f2016
+# instructor : Max Novelli (man8@pitt.edu)
+# Description:Starting with Python, Chapter 2,
+# Notes:
+# any notes to the instructor and/or TA goes here
+# ...and now let's program with Python
+
+# exercise 3
+
