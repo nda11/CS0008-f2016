2fcf74a56133e9f1ddeb504d503650e1bd2211c3
Nabil Alhassani <nda11@pitt.edu>
2016-10-07 00:31:27 -0400

    ex

--- ch4/ex3.py
+++ ch4/ex3.py
@@ -10,3 +10,27 @@
 
 # exercise 3
 
+#write a program that asks the user to enter the amount that
+# he or she has budgeted (ie has to spend) for a month.
+#Then, use a loop to prompt the user to enter his or her expenses for the month,
+#  and keep a running total. When the loop finishes, the program should display:
+#-amount budgeted
+#-the total of the entered expenses
+#- and the amount the user is under or over budget.
+
+def budget_calculas():
+    total_expanses=0
+    monthly_budget=input('enter your budget for this month:')
+    for i in range(30):
+        cmd=input('enter your expenses:')
+        total_expanses+=cmd
+        print ('total expanses:'+str(total_expanses))
+
+        total=monthly_budget-total_expanses
+        if total>0:
+            print('budget surplus:'+str(total))
+
+        else:
+            print ('budget defict:'+str(total))
+
+budget_calculas()
--- ch4/test.py
+++ ch4/test.py
@@ -1,3 +1,25 @@
-x= int(input('give me #'))
-for num in range(0,x+1):
-    print (num)
+def main():
+    # Ask the user for their budget for the month.
+    budget = input('What is your budget for the month? ')
+
+    # Create a variable to control the loop.
+    keep_going = 'y'
+
+    # Calculate a series of expenses.
+    while keep_going == 'y':
+        # Ask the user to enter an expense.
+        expense = input('Enter an expense: ')
+
+        # Calculate the remainder of the budget.
+        budget = budget - expense
+
+        # State what the remaining budget is.
+        print 'Your remaining budget is $%.2f.' % budget
+
+        # Ask the user if they'd like to enter another expense.
+        keep_going = raw_input('Do you want to calculate another ' + \
+                               'expense (Enter y for yes): ')
+
+
+# Call the main function.
+main()
