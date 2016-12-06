76608279ea9bf724930a502a5b3017c312ef26e6
nitrosx (Max Novelli) <man8@pitt.edu>
2016-12-05 23:14:30 -0500

    Fixes and correction to assignment #3

--- f2016_cs8_nda11_a3/f2016_cs8_nda11_a3.py
+++ f2016_cs8_nda11_a3/f2016_cs8_nda11_a3.py
@@ -29,7 +29,7 @@
 import os.path
 
 
-
+# MN: what does this function do?
 def read_txt(filename):
     fp = open(filename, 'r')
     # make a list of file lines
@@ -40,6 +40,7 @@ def read_txt(filename):
     return lines
 
 
+# MN: what does this function do?
 def read_csv(filename):
     # list of lines
     lines = []
@@ -65,16 +66,21 @@ def read_csv(filename):
 
 
 def main():
+    # MN: I would specify that you are asking for the master list file
     print('Please enter the name of file')
     filename = input('enter the file name:')
     # Lower input
+    # MN: this statement transform the string to all lower case but the result is lost
+    #     because you are not assigning it to a variable
+    #     Check: https://docs.python.org/3.5/library/stdtypes.html#str.lower
+    #     it states that lower method returns a copy and does not change the original string
     filename.lower()
+    # MN: why are you using a while loop on the filename
+    #     we have only one master list file, so you read it once and you are done.
     # check if file name is empty, or is q or quit
     while (filename != '' and filename != 'quit' and filename != 'q' and os.path.exists(filename)):
         # to exit the progra
         # read a list of input csv files
-
-
         csv_files = read_txt(filename)
 
         tn = 0
@@ -88,10 +94,14 @@ def main():
             for name, dist in pdata:
                 data.append((name, dist))
                 # set of distinct names
+        # MN: describe what you have in data
+        #
+        # MN: I assume that in participant you have the full list of unique names, correct?
         participants = set()
         for name, dist in data:
             participants.add(name)
-            # dict of name mapped to a list of distances
+
+        # dict of name mapped to a list of distances
         records = {}
         for person in participants:
             items = []
@@ -100,40 +110,51 @@ def main():
                 if name == person:
                     items.append(dist)
             records[person] = items
-            # minimal individual distance
+
+        # MN: this solution works, but you could combine computing min and max in one loop
+        #     reducing by one loop
+        # minimal individual distance
         min_dist_name, min_dist = data[0]
         for name, dist in data:
             if dist > min_dist:
                 min_dist = dist
                 min_dist_name = name
-                # maximum individual distance
+
+        # maximum individual distance
         max_dist_name, max_dist = data[0]
         for name, dist in data:
             if dist < max_dist:
                 max_dist = dist
                 max_dist_name = name
-                # dict of records that have more than 1 entry
+
+        # dict of records that have more than 1 entry
         multiple_records = {}
         for key, val in records.items():
             if len(val) > 1:
                 multiple_records[key] = val
                 # display the output
 
+            # MN: why are you placing the printing here with this indentation?
+            #     this would  get executed as many time as the for loop
+            #     but you interrupt with the exit() 
+            #     The correct solution would be to indent it so it is execute after the last loop
+            
+	print('Number of Input files read    : {0}'.format(len(csv_files)))
+        print('Total number of lines read    : {0}'.format(tn))
+        print('')
+        print('total distance run            : {0:11.5f}'.format(td))
+        print('')
+        print('max distance run              : {0:2.3f}'.format(min_dist))
+        print('  by participant              : {0}'.format(min_dist_name))
+        print('')
+        print('min distance run              : {0:2.3f}'.format(max_dist))
+        print('  by participant              : {0}'.format(max_dist_name))
+        print('')
+        print('Total number of participants  : {0}'.format(len(participants)))
+        print('Number of participants')
+        print(' with multiple records        : {0}'.format(len(multiple_records)))
+        print('')
+        # MN: no exit is required
+        #exit()
 
-            print('Number of Input files read    : {0}'.format(len(csv_files)))
-            print('Total number of lines read    : {0}'.format(tn))
-            print('')
-            print('total distance run            : {0:11.5f}'.format(td))
-            print('')
-            print('max distance run              : {0:2.3f}'.format(min_dist))
-            print('  by participant              : {0}'.format(min_dist_name))
-            print('')
-            print('min distance run              : {0:2.3f}'.format(max_dist))
-            print('  by participant              : {0}'.format(max_dist_name))
-            print('')
-            print('Total number of participants  : {0}'.format(len(participants)))
-            print('Number of participants')
-            print(' with multiple records        : {0}'.format(len(multiple_records)))
-            print('')
-            exit()
 main()
